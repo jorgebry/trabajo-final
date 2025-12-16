8426763a7518bd4bb0cad42da0a65907b0203344
jorgebry <jorgebryant0318@gmail.com>
2025-12-15 19:58:12 -0500

    Update and rename app.py to models.py

--- backend/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from routes.workshops import workshops_bp
-
-app = Flask(__name__)
-app.register_blueprint(workshops_bp)
-
-if __name__ == '__main__':
-    app.run(debug=True)
